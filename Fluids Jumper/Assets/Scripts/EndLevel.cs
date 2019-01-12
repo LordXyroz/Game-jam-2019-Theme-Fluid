e891dd1f27f4011bc7f10f6799f02a4bd76a39bc
Marco <maartend@stud.ntnu.no>
2019-01-12 23:54:58 +0100

    created a way to end the level after completing it, skeleton for main menu, last commit of the day

---
+++ Fluids Jumper/Assets/Scripts/EndLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    public string nextScene;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
